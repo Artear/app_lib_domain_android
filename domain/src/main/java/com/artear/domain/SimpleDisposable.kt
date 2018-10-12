e264ad9355e7060943e4690ce2288638cf12c3cf
daver1419 <daver1419@gmail.com>
2018-10-12 16:04:51 -0300

    Change: update documentation for SimpleDisposable

--- domain/src/main/java/com/artear/domain/SimpleDisposable.kt
+++ domain/src/main/java/com/artear/domain/SimpleDisposable.kt
@@ -4,7 +4,14 @@ import com.artear.tools.error.NestError
 import com.artear.tools.error.NestErrorFactory
 import io.reactivex.observers.DisposableObserver
 
-
+/**
+ * A simple observer interface between the domain and the UI.
+ *
+ * Usually used in ViewModel or Presenter to interact and connect the business logic result
+ * and serve this to presentation view.
+ *
+ * When you activity or fragment will destroyed you must to dispose this observer.
+ */
 class SimpleDisposable<T>(var onNextDelegate: (data: T) -> Unit,
                           var onErrorDelegate: (e: NestError) -> Unit) : DisposableObserver<T>() {
 
